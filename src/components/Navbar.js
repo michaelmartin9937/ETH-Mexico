e6141855e3e862d913afdcd2def5abf6711f4285
Michael Martin <michaelmartin@Michaels-MacBook-Pro.local>
2022-08-20 16:51:28 -0700

    2. Add test navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+// import { useSelector, useDispatch } from 'react-redux';
+// import Blockies from 'react-blockies';
+// import logo from '../assets/logo.png';
+// import eth from '../assets/eth.svg';
+
+// import { loadAccount } from '../store/interactions';
+
+// import config from '../config.json';
+
+
+
+const Navbar = () => {
+  
+
+  return(
+
+    <div>
+      <p>Test Navbar text</p>
+    </div>
+  )
+}
+
+export default Navbar;
+
+
